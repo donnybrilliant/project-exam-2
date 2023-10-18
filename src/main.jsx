f5d426654d9f92eca8e673cd3f58385f365691d3
donnybrilliant <daniel.vier@gmail.com>
2023-10-18 17:13:05 +0200

    Wraps App with ThemeProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
+import theme from "./utils/theme";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
