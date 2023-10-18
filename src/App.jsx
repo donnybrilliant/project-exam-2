7b3f2dfba97202495e524daa7bf8724515a4d3bc
donnybrilliant <daniel.vier@gmail.com>
2023-10-18 17:03:42 +0200

    Wraps routes in Layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Venue from "./pages/Venue";
 
 function App() {
   return (
     <Routes>
-      <Route index element={<Home />} />
-      <Route path="venues/:id" element={<Venue />} />
-      <Route path="*" element={<h1>Not Found</h1>} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="venues/:id" element={<Venue />} />
+        <Route path="*" element={<h1>Not Found</h1>} />
+      </Route>
     </Routes>
   );
 }
