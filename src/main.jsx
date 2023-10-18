a5be949ca9592b3d136bb4edc803bf330552983d
donnybrilliant <daniel.vier@gmail.com>
2023-10-18 17:25:38 +0200

    Adds CssBaseline

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "@mui/material";
-import { BrowserRouter } from "react-router-dom";
 import theme from "./utils/theme";
+import { CssBaseline } from "@mui/material";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <BrowserRouter>
         <App />
       </BrowserRouter>
