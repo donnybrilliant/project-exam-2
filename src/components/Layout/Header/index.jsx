bb4e5a215999e90201c02146667dc2425d45e7e8
donnybrilliant <daniel.vier@gmail.com>
2023-10-19 12:33:03 +0200

    Creates simple Header

--- src/components/Layout/Header/index.jsx
+++ src/components/Layout/Header/index.jsx
@@ -1,5 +1,18 @@
-const Header = () => {
-  return <div>Header</div>;
-};
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Logo from "../../ui/Logo";
 
+const Header = () => {
+  return (
+    <AppBar position="sticky" color="white">
+      <Toolbar sx={{ justifyContent: "space-between" }}>
+        <Logo fontSize="large" />
+        <Typography sx={{ flexGrow: 1 }}>Holidaze</Typography>
+        <Button variant="contained">Login</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
 export default Header;
