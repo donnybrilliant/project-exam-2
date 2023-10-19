a07619e7fc7411215c89e9e273d6a3af18c3df42
donnybrilliant <daniel.vier@gmail.com>
2023-10-19 21:33:51 +0200

    Creates dark/light mode toggle

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "@mui/material";
-import theme from "./utils/theme";
+
+import { ThemeProviderComponent } from "./utils/theme";
 import { CssBaseline } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
+    <ThemeProviderComponent>
       <CssBaseline />
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </ThemeProvider>
+    </ThemeProviderComponent>
   </React.StrictMode>
 );
