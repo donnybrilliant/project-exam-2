038fc83b5198923d3793b731946345320211852c
donnybrilliant <daniel.vier@gmail.com>
2023-10-19 14:37:26 +0200

    Adds VenueList to Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,6 +2,8 @@ import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import useAuthStore from "../../stores/authStore";
 import useVenuesStore from "../../stores/venuesStore";
+import VenueList from "../../components/VenueList";
+import Container from "@mui/material/Container";
 
 // Home page component
 const Home = () => {
@@ -20,14 +22,19 @@ const Home = () => {
   if (isError) return <h1>Error</h1>;
 
   return (
-    <ul>
+    <Container>
+      <VenueList venues={venues} />
+    </Container>
+    /*     <ul>
       {Array.isArray(venues) &&
         venues.map((venue) => (
           <li key={venue.id}>
-            <Link to={`/venues/${venue.id}`}>{venue.name}</Link>
+            <Link to={`/venues/${venue.id}`}>
+              {venue.name} {venue.rating}
+            </Link>
           </li>
         ))}
-    </ul>
+    </ul> */
   );
 };
 
