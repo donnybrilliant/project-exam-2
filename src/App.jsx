96a12ae5be0fc19b0c7b84f17ad3edcfaa89b796
donnybrilliant <daniel.vier@gmail.com>
2023-10-20 12:14:55 +0200

    Sets up login and register routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import VenuePage from "./pages/Venue";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="venues/:id" element={<VenuePage />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
         <Route path="*" element={<h1>Not Found</h1>} />
       </Route>
     </Routes>
