b6feb2cf21051c121e19e606823bb1cc47bea1cd
donnybrilliant <daniel.vier@gmail.com>
2023-10-21 16:02:21 +0200

    Adds Logout button

--- src/components/Layout/Header/index.jsx
+++ src/components/Layout/Header/index.jsx
@@ -4,8 +4,23 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Logo from "../../ui/Logo";
 import { NavLink } from "react-router-dom";
+import { useAuthStore } from "../../../stores/index";
+import { useNavigate } from "react-router-dom";
 
+// This component is used to display a header with a logo and a login button
 const Header = () => {
+  const token = useAuthStore((state) => state.token);
+  const clearAuthInfo = useAuthStore((state) => state.clearAuthInfo);
+  const navigate = useNavigate();
+
+  // This function is used to clear the auth info and navigate to the home page
+  const handleLogout = (event) => {
+    event.preventDefault();
+    clearAuthInfo();
+    // or navigate elsewhere?
+    navigate("/");
+  };
+
   return (
     <AppBar position="sticky" color="white" sx={{ marginBottom: 4 }}>
       <Toolbar sx={{ justifyContent: "space-between" }}>
@@ -13,9 +28,17 @@ const Header = () => {
         <Typography component={NavLink} to={"/"} sx={{ flexGrow: 1 }}>
           Holidaze
         </Typography>
-        <Button component={NavLink} to={"/login"} variant="contained">
-          Login
-        </Button>
+        {!token ? (
+          <Button component={NavLink} to={"/login"} variant="contained">
+            Login
+          </Button>
+        ) : (
+          <>
+            <Button variant="contained" onClick={handleLogout}>
+              Logout
+            </Button>
+          </>
+        )}
       </Toolbar>
     </AppBar>
   );
