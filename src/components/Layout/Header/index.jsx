c537b4cd8b1b29aca798d7f4eb30e673513dfe2c
donnybrilliant <daniel.vier@gmail.com>
2023-10-21 16:54:47 +0200

    Adds link to dashboard

--- src/components/Layout/Header/index.jsx
+++ src/components/Layout/Header/index.jsx
@@ -6,10 +6,12 @@ import Logo from "../../ui/Logo";
 import { NavLink } from "react-router-dom";
 import { useAuthStore } from "../../../stores/index";
 import { useNavigate } from "react-router-dom";
+import Avatar from "@mui/material/Avatar";
 
 // This component is used to display a header with a logo and a login button
 const Header = () => {
   const token = useAuthStore((state) => state.token);
+  const userInfo = useAuthStore((state) => state.userInfo);
   const clearAuthInfo = useAuthStore((state) => state.clearAuthInfo);
   const navigate = useNavigate();
 
@@ -25,8 +27,8 @@ const Header = () => {
     <AppBar position="sticky" color="white" sx={{ marginBottom: 4 }}>
       <Toolbar sx={{ justifyContent: "space-between" }}>
         <Logo fontSize="large" />
-        <Typography component={NavLink} to={"/"} sx={{ flexGrow: 1 }}>
-          Holidaze
+        <Typography sx={{ flexGrow: 1 }}>
+          <NavLink to={"/"}>Holidaze</NavLink>
         </Typography>
         {!token ? (
           <Button component={NavLink} to={"/login"} variant="contained">
@@ -34,6 +36,13 @@ const Header = () => {
           </Button>
         ) : (
           <>
+            <Typography>{userInfo.name}</Typography>
+            <Avatar
+              component={NavLink}
+              to={"/dashboard"}
+              alt={userInfo.name}
+              src={userInfo.avatar}
+            />
             <Button variant="contained" onClick={handleLogout}>
               Logout
             </Button>
