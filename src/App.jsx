70a5e79966d435c8c9ad636360a288a8c6e9a932
donnybrilliant <daniel.vier@gmail.com>
2023-10-21 16:01:45 +0200

    Adds PublicOnlyRoute to Login and Register

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import VenuePage from "./pages/Venue";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import PublicOnlyRoute from "./components/PublicOnlyRoute"; // Add PublicRoute
 
 function App() {
   return (
@@ -11,8 +12,23 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="venues/:id" element={<VenuePage />} />
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
+        <Route
+          path="login"
+          element={
+            <PublicOnlyRoute>
+              <Login />
+            </PublicOnlyRoute>
+          }
+        />
+        <Route
+          path="register"
+          element={
+            <PublicOnlyRoute>
+              <Register />
+            </PublicOnlyRoute>
+          }
+        />
+
         <Route path="*" element={<h1>Not Found</h1>} />
       </Route>
     </Routes>
