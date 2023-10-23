4ff93c6d61e44993ae439f966ba227473590aa8a
donnybrilliant <daniel.vier@gmail.com>
2023-10-23 21:11:41 +0200

    Adds range to DateCalendar

--- src/pages/Venue/index.jsx
+++ src/pages/Venue/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams, useLocation, useNavigate } from "react-router-dom";
 import { useVenueStore, useAuthStore, useFetchStore } from "../../stores";
 import ImageGallery from "../../components/ImageGallery";
@@ -22,8 +22,11 @@ import PetsIcon from "@mui/icons-material/Pets";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import { PickersDay } from "@mui/x-date-pickers/PickersDay";
+
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
+import { useTheme } from "@mui/material/styles";
 dayjs.extend(utc);
 
 // Venue page component
@@ -36,6 +39,79 @@ const VenuePage = () => {
   const fetchVenueById = useVenueStore((state) => state.fetchVenueById);
   const isLoading = useFetchStore((state) => state.isLoading);
   const isError = useFetchStore((state) => state.isError);
+  const [dateRange, setDateRange] = useState([null, null]);
+
+  const checkDisabledDatesInRange = (startDate, endDate) => {
+    for (let d = startDate; d <= endDate; d = dayjs(d).add(1, "day")) {
+      if (
+        selectedVenue?.bookings.some(
+          (booking) =>
+            d.isAfter(
+              dayjs.utc(booking.dateFrom).subtract(1, "day").startOf("day")
+            ) && d.isBefore(dayjs.utc(booking.dateTo).startOf("day"))
+        )
+      ) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const handleDateClick = (date) => {
+    setDateRange((prevRange) => {
+      const startDate = prevRange[0];
+      const endDate = date;
+
+      // Set start date if no start date is selected
+      if (!startDate) {
+        return [date, null];
+      } else if (
+        // Reset start date if end date is clicked twice
+        prevRange[1] &&
+        dayjs(prevRange[1]).isSame(dayjs(date), "day")
+      ) {
+        return [date, null];
+      }
+      // Set new start date if selected date is before the current start date
+      if (dayjs(date).isBefore(dayjs(startDate), "day")) {
+        return [date, null];
+      }
+      // If the selected date is in the disabled range, set it as the new start date
+      if (checkDisabledDatesInRange(startDate, date)) {
+        return [date, null];
+      }
+
+      // Reset start date if new start date and end date are the same
+      if (dayjs(startDate).isSame(dayjs(endDate), "day")) {
+        return [startDate, null];
+      }
+      // Extend range if two dates already selected
+      if (prevRange[1]) {
+        return [startDate, endDate];
+      }
+
+      return [startDate, endDate];
+    });
+  };
+
+  // Custom day component to highlight days in calendar
+  const CustomDay = (props) => {
+    const theme = useTheme();
+    const inRange =
+      dateRange[0] &&
+      dateRange[1] &&
+      dayjs(props.day).isBetween(dateRange[0], dateRange[1], "day", "[]");
+
+    const matchedStyles = inRange
+      ? {
+          backgroundColor: theme.palette.primary.main,
+          color: theme.palette.common.white,
+        }
+      : {};
+    return <PickersDay {...props} sx={{ ...matchedStyles }} />;
+  };
+
+  //console.log(dateRange);
 
   const location = useLocation();
   const navigate = useNavigate();
@@ -48,7 +124,7 @@ const VenuePage = () => {
   if (isLoading) return <h1>Loading...</h1>;
   if (isError) return <h1>Error</h1>;
 
-  console.log(selectedVenue);
+  //console.log(selectedVenue);
 
   const navigateToLogin = () => {
     navigate("/login", { state: { from: location } });
@@ -116,7 +192,10 @@ const VenuePage = () => {
         </CardContent>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <DateCalendar
+            onChange={handleDateClick}
+            slots={{ day: CustomDay }}
             disablePast
+            value={dateRange[0]}
             shouldDisableDate={(day) => {
               return selectedVenue?.bookings.some(
                 (booking) =>
