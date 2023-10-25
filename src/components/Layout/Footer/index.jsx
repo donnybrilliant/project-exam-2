497f1c937306349ec3be939f664c4b2045d6a9a1
donnybrilliant <daniel.vier@gmail.com>
2023-10-25 14:49:51 +0200

    Cleans up Footer

--- src/components/Layout/Footer/index.jsx
+++ src/components/Layout/Footer/index.jsx
@@ -1,19 +1,10 @@
-import { useThemeStore } from "../../../stores";
-import { Toolbar, Typography, IconButton } from "@mui/material";
-import { DarkMode } from "@mui/icons-material";
-import { LightMode } from "@mui/icons-material";
+import { Toolbar, Typography } from "@mui/material";
 
 // This component is used to display a footer with a toggle color mode button
 const Footer = () => {
-  const toggleColorMode = useThemeStore((state) => state.toggleColorMode);
-  const colorMode = useThemeStore((state) => state.colorMode);
-
   return (
     <Toolbar sx={{ marginTop: 4, justifyContent: "space-between" }}>
       <Typography>Footer</Typography>
-      <IconButton onClick={toggleColorMode} aria-label="change-color-mode">
-        {colorMode === "light" ? <DarkMode /> : <LightMode />}
-      </IconButton>
     </Toolbar>
   );
 };
