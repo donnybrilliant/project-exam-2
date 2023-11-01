fa11027ea91ecae8779b5cac30fa09c8fcf43b9d
donnybrilliant <daniel.vier@gmail.com>
2023-11-01 15:36:28 +0100

    Creates OwnerRoute, AccessControl component

--- src/components/AccessControl/index.jsx
+++ src/components/AccessControl/index.jsx
@@ -1,11 +1,13 @@
-import { Navigate, useLocation } from "react-router-dom";
-import { useAuthStore } from "../../stores";
+import { useEffect } from "react";
+import { Navigate, useLocation, useNavigate } from "react-router-dom";
+import { useAuthStore, useVenueStore } from "../../stores";
 
 // This component is used to display a public only route, rerouting to the home page if the user is logged in
 export const PublicOnlyRoute = ({ children }) => {
   const token = useAuthStore((state) => state.token);
   return token ? <Navigate to="/" replace /> : children;
 };
+
 // This component is used to display a private only route, rerouting to the login page if the user is not logged in
 export const PrivateRoute = ({ children }) => {
   const token = useAuthStore((state) => state.token);
@@ -15,4 +17,20 @@ export const PrivateRoute = ({ children }) => {
   ) : (
     children
   );
+};
+
+// This component is used to display a owner only route, rerouting to the home page if the user is not an owner
+export const OwnerRoute = ({ children }) => {
+  const navigate = useNavigate();
+  const isOwner = useVenueStore((state) => state.isOwner());
+  const { pathname } = useLocation(); // Get the current path
+
+  useEffect(() => {
+    if (!isOwner) {
+      const venueId = pathname.split("/")[2]; // Assuming the path is /venues/:id/edit
+      navigate(`/venues/${venueId}`); // Redirect to the venue page if not the owner
+    }
+  }, [isOwner, navigate, pathname]);
+
+  return isOwner ? children : null;
 };
