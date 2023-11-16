a5980b359136574ddb8a881b78d385ce1e20d56f
donnybrilliant <daniel.vier@gmail.com>
2023-11-16 14:19:43 +0100

    Changes logic on rendering VenueList

--- src/components/VenueList/index.jsx
+++ src/components/VenueList/index.jsx
@@ -10,54 +10,63 @@ import { VenueCardSkeleton } from "../Skeletons";
 const VenueList = ({ venues }) => {
   const isLoading = useFetchStore((state) => state.isLoading);
 
-  if (venues) {
+  if (isLoading) {
     return (
       <>
-        <Container
-          disableGutters
-          sx={{
-            display: "flex",
-            alignItems: "flex-end",
-            flexWrap: "nowrap", // prevent wrapping by default
-            justifyContent: "space-between", // this will space out your SortAndFilter and Typography
-            "@media (max-width: 380px )": {
-              flexWrap: "wrap", // allow wrapping when the viewport width is less than a certain breakpoint
-            },
-          }}
-        >
-          <SortAndFilter />
-          <Typography
-            variant="body2"
-            sx={{
-              minWidth: "130px",
-              marginBottom: 1.5,
-              "@media (max-width: 380px)": {
-                width: "100%", // full width on smaller screens to ensure it goes to the next line
-
-                marginTop: 1, // add some space above when on its own line
-              },
-              textAlign: "right", // center it if you'd like
-            }}
-          >
-            {venues.length} venues found
-          </Typography>
-        </Container>
+        <SortAndFilter />
         <Grid container spacing={4}>
-          {isLoading
-            ? Array.from(new Array(9)).map((item, index) => (
-                <VenueCardSkeleton key={index} />
-              ))
-            : venues.map((venue) => (
-                <Grid item key={venue.id} xs={12} sm={6} md={4}>
-                  <VenueCard venue={venue} />
-                </Grid>
-              ))}
+          {Array.from(new Array(9)).map((item, index) => (
+            <VenueCardSkeleton key={index} />
+          ))}
         </Grid>
       </>
     );
-  } else if (venues && venues.length === 0) {
-    return <Typography>No venues found</Typography>;
   }
+
+  if (!venues || venues.length === 0) {
+    return <Typography>No venues found. Try another search!</Typography>;
+  }
+
+  return (
+    <>
+      <Container
+        disableGutters
+        sx={{
+          display: "flex",
+          alignItems: "flex-end",
+          flexWrap: "nowrap", // prevent wrapping by default
+          justifyContent: "space-between", // this will space out your SortAndFilter and Typography
+          "@media (max-width: 380px )": {
+            flexWrap: "wrap", // allow wrapping when the viewport width is less than a certain breakpoint
+          },
+        }}
+      >
+        <SortAndFilter />
+        <Typography
+          variant="body2"
+          sx={{
+            minWidth: "130px",
+            marginBottom: 1.5,
+            "@media (max-width: 380px)": {
+              width: "100%", // full width on smaller screens to ensure it goes to the next line
+
+              marginTop: 1, // add some space above when on its own line
+            },
+            textAlign: "right", // center it if you'd like
+          }}
+        >
+          {venues.length} venues found
+        </Typography>
+      </Container>
+      <Grid container spacing={4}>
+        {venues.map((venue) => (
+          <Grid item key={venue.id} xs={12} sm={6} md={4}>
+            <VenueCard venue={venue} />
+          </Grid>
+        ))}
+      </Grid>
+    </>
+  );
 };
 
 export default VenueList;
