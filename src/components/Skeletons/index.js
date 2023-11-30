8436bd654ba59bdd19a71355fec7df3d67fb3d67
donnybrilliant <daniel.vier@gmail.com>
2023-11-30 14:02:44 +0100

    Creates ListSkeleton

--- src/components/Skeletons/index.js
+++ src/components/Skeletons/index.js
@@ -1,2 +1,3 @@
 export { default as VenueCardSkeleton } from "./VenueCardSkeleton";
 export { default as VenuePageSkeleton } from "./VenuePageSkeleton";
+export { default as ListSkeleton } from "./ListSkeleton";
