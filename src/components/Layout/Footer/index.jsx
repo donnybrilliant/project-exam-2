4e49fc50f93abb9e95f68cc4959045d8cec36996
donnybrilliant <daniel.vier@gmail.com>
2023-12-01 13:43:37 +0100

    Creates content in Footer

--- src/components/Layout/Footer/index.jsx
+++ src/components/Layout/Footer/index.jsx
@@ -1,11 +1,41 @@
-import { Toolbar, Typography } from "@mui/material";
+import { Toolbar, Typography, Container } from "@mui/material";
 
 // This component is used to display a footer with a toggle color mode button
 const Footer = () => {
   return (
-    <Toolbar sx={{ marginTop: 4, justifyContent: "space-between" }}>
-      <Typography>Footer</Typography>
-    </Toolbar>
+    <footer>
+      <Container maxWidth="false">
+        <Typography
+          sx={{
+            textAlign: "center",
+            fontSize: "20vw",
+            opacity: 0.1,
+          }}
+        >
+          Holidaze
+        </Typography>
+        <Toolbar
+          sx={{
+            mt: 10,
+            mb: 2,
+            flexDirection: "column",
+            color: "text.secondary",
+          }}
+        >
+          <Typography variant="body2">
+            All bookings on Holidaze are for whole days.
+          </Typography>
+          <Typography variant="body2">
+            You can check in at 00.01 and check out 23.59 - if you want.
+          </Typography>
+          <Typography variant="body2">
+            We can't be contacted - contact the venue owner directly after
+            booking.
+          </Typography>
+          <Typography variant="body2">No privacy, no policy.</Typography>
+        </Toolbar>
+      </Container>
+    </footer>
   );
 };
 
