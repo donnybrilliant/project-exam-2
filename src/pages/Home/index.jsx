7b6db616a5336c64a7a8d6aa8dacf5760c282b00
donnybrilliant <daniel.vier@gmail.com>
2023-12-03 09:49:08 +0100

    Improves styling on Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,7 +6,7 @@ import Search from "../../components/Search";
 import VenueCarousel from "../../components/VenueCarousel";
 import Box from "@mui/material/Box";
 import { LogoIcon } from "../../components/Layout/Header/Logo";
-
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import Paper from "@mui/material/Paper";
 
 // This component is used to display a home page with a list of venues
@@ -50,7 +50,7 @@ const Home = () => {
             flexWrap: "wrap",
             justifyContent: "center",
             gap: 3,
-            my: 20,
+            my: { xs: 5, sm: 20 },
           }}
         >
           <Box
@@ -81,24 +81,56 @@ const Home = () => {
           />
         </Box>
 
-        <Typography variant="h4" component="h2" marginTop={5} gutterBottom>
-          Top Rated Venues
-        </Typography>
+        <Box
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-between"
+          alignItems="center"
+          sx={{ mt: 15 }}
+        >
+          <Typography variant="h4" component="h2">
+            Top Rated Venues
+          </Typography>
+          <ChevronRightIcon />
+        </Box>
         <VenueCarousel venues={topRatedVenues} />
-        <Typography variant="h4" component="h2" marginTop={5} gutterBottom>
-          Most Popular Venues
-        </Typography>
+        <Box
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-between"
+          alignItems="center"
+          sx={{ mt: 5 }}
+        >
+          <Typography variant="h4" component="h2">
+            Most Popular Venues
+          </Typography>
+          <ChevronRightIcon />
+        </Box>
         <VenueCarousel venues={mostPopularVenues} />
-        <Typography variant="h4" component="h2" marginTop={5} gutterBottom>
-          Newest Venues
-        </Typography>
+        <Box
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-between"
+          alignItems="center"
+          sx={{ mt: 5 }}
+        >
+          <Typography variant="h4" component="h2">
+            Newest Venues
+          </Typography>
+          <ChevronRightIcon />
+        </Box>
         <VenueCarousel venues={newVenues} />
       </Container>
 
       <Container
         disableGutters
         maxWidth="false"
-        sx={{ my: 10, p: 10, backgroundColor: "primary.main" }}
+        sx={{
+          my: 20,
+          py: 10,
+          px: { xs: 4, sm: 20 },
+          backgroundColor: "primary.main",
+        }}
       >
         <Typography variant="h5" gutterBottom>
           Your Perfect Getaway is Just a Click Away!
@@ -109,8 +141,14 @@ const Home = () => {
           bustling city escape, Holidaze is your go-to platform for a tailored
           holiday experience.
         </Typography>
-        <Button variant="contained" color="primary" size="large" sx={{ mt: 3 }}>
-          Discover Top Venues
+        <Button
+          variant="contained"
+          color="info"
+          size="large"
+          sx={{ mt: 3 }}
+          href="/venues"
+        >
+          Discover Venues
         </Button>
       </Container>
       <Container
@@ -119,6 +157,7 @@ const Home = () => {
           gap: 4,
           justifyContent: "space-between",
           flexWrap: "wrap",
+          my: 20,
         }}
       >
         <Paper
@@ -138,7 +177,12 @@ const Home = () => {
             Our easy-to-navigate platform opens up a world of venues for you to
             explore and book. Your next holiday is just a browse away.
           </Typography>
-          <Button variant="contained" color="primary" size="large">
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            href="/venues"
+          >
             Browse Venues
           </Button>
         </Paper>
@@ -160,7 +204,12 @@ const Home = () => {
             personal dashboard to view, edit, or cancel your bookings anytime,
             anywhere.
           </Typography>
-          <Button variant="contained" color="primary" size="large">
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            href="/dashboard"
+          >
             Go to Dashboard
           </Button>
         </Paper>
@@ -181,7 +230,12 @@ const Home = () => {
             Are you a venue owner? Join Holidaze to list your venue, manage
             bookings, and connect with potential customers effortlessly.
           </Typography>
-          <Button variant="contained" color="primary" size="large">
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            href="/venuemanager/register"
+          >
             Register Venue
           </Button>
         </Paper>
